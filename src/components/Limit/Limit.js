f1ea2477aa8c14bc92f90882b4fcf0c94e9c96b5
kamoe <fran4ecko1990@yandex.ru>
2021-08-09 19:25:37 +0300

    fix button_page_html

--- src/components/Limit/Limit.js
+++ src/components/Limit/Limit.js
@@ -11,19 +11,28 @@ class Limit extends Component {
           startingAmount: "",
           depositInterest: "",
           taskResult: "",
+          NameError: "",
+          isValid: false,
         };
       }
     
       handlerChange = (event) => {
         let name = event.target.name; //получаем название поля
-        let value = event.target.value; // получаем значение поля
-  
-        this.setState({ 
-          [name]: value,
-          taskResult: this.resultInput(name, value)
-        });
-        // console.log(this.state);
+        let value = Number(event.target.value); // получаем значение поля
 
+        if (!isNaN(value)) {
+          this.setState({ 
+            [name]: value,
+            taskResult: this.resultInput(name, value),
+            NameError: "",
+            isValid: true,
+          });
+        } else {
+          this.setState({
+            NameError: "Необходимо указывать числовые данные",
+            isValid: false,
+          })
+        }
       };
     
       handlerSubmit(event) {
@@ -34,18 +43,23 @@ class Limit extends Component {
           console.log("Вы не заполнили поля");
         }
       }
-
+      // https://law03.ru/kalkulyator/nakoplenij_deneg 
       resultInput(name, value) {
-        let data = {...this.state, [name]: value}
-        console.log(data)
+        let data = {...this.state, [name]: value};
+        let persent = data.depositInterest / 100; // Расчет процента
+        let resultSum1 = data.requiredAmount - data.startingAmount * (1 + persent / 12)**data.targetTerm;
+        let resultsum2 = (1 + persent / 12)**data.targetTerm - 1;
+        let resultsum3 = persent / 12 * (resultSum1 / resultsum2)
+        return resultsum3.toFixed(2);
       }
 
     render() { 
         return (
             <div className="purpose_made">
-                
+              <div className="error">{this.state.NameError}</div>
                 <form onSubmit={this.handlerSubmit}>
                     <label>
+                        Сумма для достижения цели 
                         <input
                         name="requiredAmount"
                         type="text"
@@ -54,46 +68,47 @@ class Limit extends Component {
                         placeholder="Необходимая сумма"
                         />
                     </label>
-
                     <label>
+                        Количество месяцев
                         <input
                         name="targetTerm"
-                        type="number"
+                        type="text"
                         value={this.state.targetTerm}
                         onChange={this.handlerChange}
                         placeholder="Срок достижения"
                         />
                     </label>
-
                     <label>
+                        Начальная сумма вложений
                         <input
                         name="startingAmount"
-                        type="number"
+                        type="text"
                         value={this.state.startingAmount}
                         onChange={this.handlerChange}
                         placeholder="Стартовая сумма"
                         />
                     </label>
-
                     <label>
+                        Ежемесячное начисление процентов
                         <input
                         name="depositInterest"
-                        type="number"
+                        type="text"
                         value={this.state.depositInterest}
                         onChange={this.handlerChange}
                         placeholder="Процент по вкладу"
                         />
                     </label>
-
                     <label>
+                        Сумма ежемесячного пополнения
                         <input
                         name="taskResult"
                         type="text"
                         value={this.state.taskResult}
+                        placeholder="Сумма ежемесячного платежа"
                         />
                     </label>
-                <input type="submit" value="Создать цель" />
-                <button>Вернуться назад</button>
+                <button type="submit" className="buttonBox">Создать цель</button>
+                <button className="buttonBox">Вернуться назад</button>
                 </form>
             </div>
         );
