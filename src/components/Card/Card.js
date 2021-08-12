167d6a12941cbc71dd83b670111517e1852c7ef6
Aida IdriSova <aida-107@mail.ru>
2021-08-12 19:31:55 +0300

    main

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,28 +1,28 @@
-import {Component} from 'react';
-//import store from '../../reducer/store';
-//import {input} from '../components/action/CardAction'
-import Limit from '../Limit/Limit';
-import {onSubmit} from '../Limit/Limit';
-class Card extends Component {
+// import {Component} from 'react';
+// //import store from '../../reducer/store';
+// //import {input} from '../components/action/CardAction'
+// import Limit from '../Limit/Limit';
+// //import {onSubmit} from '../Limit/Limit';
+// class Card extends Component {
 
-onSubmit = (cardPart) => {
-    store.dispatch({
-        type: add,
-        payload: cardPart
+// // onSubmit = (cardPart) => {
+// //     store.dispatch({
+// //         type: add,
+// //         payload: cardPart
         
-    })
-}
+// //     })
+// // }
 
-    render() {
-        const {text, requiredAmount, targetTerm, startingAmount, depositInterest  } = this.props
-        return (
-            <div>
-                <div>{text}</div>
-                <div>{requiredAmount}</div>
+//     render() {
+//         const {text, requiredAmount, targetTerm, startingAmount, depositInterest  } = this.props
+//         return (
+//             <div>
+//                 <div>{text}</div>
+//                 <div>{requiredAmount}</div>
 
-            </div>
-        )
-    }
-}
-export default Card;
+//             </div>
+//         )
+//     }
+// }
+// export default Card;
 
