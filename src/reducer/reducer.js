dfa349b13925cf45d786a99bd4f0f839d5a612da
Aida IdriSova <aida-107@mail.ru>
2021-08-13 15:41:53 +0300

    main

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -3,8 +3,20 @@ import {input} from '../components/action/CardAction'
 // import {toggle} from '../components/action/CardAction'
 
 let initialState = { 
-    cardItem: [],
-    cardlist: []
+    cardlist: [
+        {
+        requiredAmount: "300000",
+        targetTerm: "6",
+        startingAmount: "0",
+        depositInterest: "5"
+        },
+        {
+            requiredAmount: "200000",
+            targetTerm: "6",
+            startingAmount: "0",
+            depositInterest: "5"
+            }
+    ],
     
 }
 
@@ -15,11 +27,12 @@ function reducer(state = initialState, action) {
     // 1. Добавить в массив целей новую цель (пока добавить только название)
     // 2. Проверить какой последний id у последней цели и добавить к новой цели уникалькальный id.
     if(action.type === input) {  //ввод новых значений в форму 
-    let card = action.payload
-    console.log(card)
-      return ({...state.cardItem, card})
-    // let card = state.cardItem.find(item => item.id === action.cardlist)
+    // let card = action.payload
     // console.log(card)
+    //   return ({...state.cardItem, card})
+        return {
+            ...state, cardlist: state.cardlist.push(action.payload)
+        }
     }  
    
       return state;
