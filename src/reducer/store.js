652bb953f5d453f43a414fcfd19acad1ae4b5d4e
kamoe <fran4ecko1990@yandex.ru>
2021-08-23 19:38:46 +0300

    fix_server_target_qal

--- src/reducer/store.js
+++ src/reducer/store.js
@@ -1,6 +1,8 @@
-import {createStore}  from 'redux';
+import {createStore, applyMiddleware }  from 'redux';
 import reducer from './reducer';
+import thunk from 'redux-thunk';
 
+// const store = createStore(reducer, applyMiddleware(thunk));
 let store = createStore(reducer);
 
 export default store;
