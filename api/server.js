c41bec3a8f769b053792e48d9004b9c66a6099f4
kamoe <fran4ecko1990@yandex.ru>
2021-08-28 11:39:21 +0300

    fix

--- api/server.js
+++ api/server.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+const config = require("../src/config/config")
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '12345678',
-    database: 'bootcamptarget'
+    host: config.host,
+    port: config.port,
+    user: config.user,
+    password: config.password,
+    database: config.database
 })
 
 connection.connect((err) => {
@@ -18,7 +25,6 @@ connection.connect((err) => {
         console.log('Мы подключились к БД');
     }
 })
-
 
 app.get('/purpose/all', (request, response) => {
     
@@ -33,26 +39,38 @@ app.get('/purpose/all', (request, response) => {
     })
 })
 
-app.post('/purpose/:id', (request, response) => {
+app.post('/purpose/add', (request, response) => {
     
-    response.json(request.body);
-    const {nameTarget, requiredAmount, targetTerm, startingAmount, depositInterest, taskResult} = request.body;
-
+    const {id, nameTarget, requiredAmount, targetTerm, startingAmount, depositInterest, taskResult} = request.body;
+    
     connection.query(`
-        INSER INTO targets (nameTarget, requiredAmount, targetTerm, startingAmount, depositInterest, taskResult)
-        VALUES (${nameTarget}, ${requiredAmount}, ${targetTerm}, ${startingAmount}, ${depositInterest}, ${taskResult});
+        INSERT INTO targets (nameTarget, requiredAmount, targetTerm, startingAmount, depositInterest, taskResult)
+        VALUES ('${nameTarget}', ${requiredAmount}, ${targetTerm}, ${startingAmount}, ${depositInterest}, ${taskResult});
         `, 
     (err, data) => {
         if(err) {
-            response.status(404).json('not found')
+            response.status(401).json('not add')
+            return;
         }
         response.setHeader('Access-Control-Allow-Origin', "*");
         response.status(200).json(data);
     })
 })
 
-app.put(() => {
-    
+app.put('/purpose/EditPage/:id', (request, response) => {
+    const {nameTarget, requiredAmount, targetTerm, startingAmount, depositInterest, taskResult} = request.body;
+
+    connection.query(`UPDATE targets SET (nameTarget, requiredAmount, targetTerm, startingAmount, depositInterest, taskResult)
+    VALUES ('${nameTarget}', ${requiredAmount}, ${targetTerm}, ${startingAmount}, ${depositInterest}, ${taskResult});`, 
+    (err, data) => {
+        if(err) {
+            response.status(401).status('Не получилось обновить')
+            return
+        }
+        response.setHeader('Access-Control-Allow-Origin', "*");
+        response.status(200).json(data);
+    })
+
 })
 
 app.listen(3010, () => {
