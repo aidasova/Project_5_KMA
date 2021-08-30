d5b109755a2ab8db648395110189148e61bdb9bc
Aleksei Sokolov <aleksei.sokolov.85@mail.ru>
2021-08-30 19:36:42 +0300

    added cors library

--- api/server.js
+++ api/server.js
@@ -2,14 +2,24 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const config = require("../src/config/config")
+const cors = require('cors')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// по дефолту значения CORS такие: 
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+// }
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 
 const connection = mysql.createConnection({
